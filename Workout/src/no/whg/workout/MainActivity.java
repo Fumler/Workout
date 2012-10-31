6f01b23514314ad0754d6249f61d2942f765c28e
Fredrik Pettersen <fredrik.fumler@gmail.com>
2012-10-31 21:14:06 +0100

    Enabled sexy-mode

--- Workout/src/no/whg/workout/MainActivity.java
+++ Workout/src/no/whg/workout/MainActivity.java
@@ -1,19 +1,14 @@
 package no.whg.workout;
 
-import android.app.ActionBar;
-import android.app.FragmentTransaction;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
