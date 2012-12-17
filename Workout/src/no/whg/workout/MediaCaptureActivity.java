71ea09729837ced9f2ad1296eb2fb70d7f575c1e
Snorre Brecheisen <snott_on_a_roll@hotmail.com>
2012-12-17 17:33:57 +0100

    Cleared unused stuff out of MediaCaptureActivity. Should be complete at this point.

--- Workout/src/no/whg/workout/MediaCaptureActivity.java
+++ Workout/src/no/whg/workout/MediaCaptureActivity.java
@@ -3,16 +3,12 @@ package no.whg.workout;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.view.View;
-import android.widget.ImageButton;
 
 public class MediaCaptureActivity extends Activity {
 	private static final int IMAGE_REQUEST_CODE = 100;
